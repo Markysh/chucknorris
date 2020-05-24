5aa44932984fdd3a18ab8bfe56e126f27d5cefe7
Olya Markysh <december.olka51@gmail.com>
2020-05-24 11:51:44 +0300

    deploy

---
+++ build/precache-manifest.426d31e3bf3f39b5d2f2f751be9625e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d308f1d9b98e6f1adfae4a85ad99f9ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32b0218d87a219cb55bf",
+    "url": "/static/js/2.c3c2ae72.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c3c2ae72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "650380f60f71e20d94b9",
+    "url": "/static/js/main.d032d414.chunk.js"
+  },
+  {
+    "revision": "a0aba3e30e192dbd083d",
+    "url": "/static/js/runtime-main.419d0b83.js"
+  },
+  {
+    "revision": "6332643400d59a0cc8bac831c3bf19b7",
+    "url": "/static/media/Roboto-Bold.63326434.ttf"
+  },
+  {
+    "revision": "9dda4e591e6d4bce5d4a8e2a195dca07",
+    "url": "/static/media/Roboto-Medium.9dda4e59.ttf"
+  },
+  {
+    "revision": "90679e0cfe8c59c45c061a82c48f01d4",
+    "url": "/static/media/Roboto-Regular.90679e0c.ttf"
+  }
+]);
